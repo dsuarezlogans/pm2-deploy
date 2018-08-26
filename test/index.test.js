97dadde101010ed10c3c5b91303d34cab8333928
Danny Suarez <dsuarezlogans@gmail.com>
2018-08-26 17:34:22 -0500

    fix test loop

--- test/index.test.js
+++ test/index.test.js
@@ -6,18 +6,23 @@ const app = require('../index.js');
 const expected = 2;
 
 describe('My first test', () => {
-  it('should pass test', () => {
+  it('should pass test', (done) => {
     expect(2).to.equal(expected);
+    done();
   });
-  it('should fail test', () => {
+  it('should fail test', (done) => {
     expect(4).to.not.equal(expected);
+    done();
   });
 });
 
 describe('GET /', () => {
-  it('should respond with 200', () => {
+  it('should respond with 200', (done) => {
     request(app)
       .get('/')
-      .end((err, res) => expect(res.status).to.equal(200));
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
   });
 });
