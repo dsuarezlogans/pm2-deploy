c7106160334c41ae2b7237a475b835d216b03e87
Danny Suarez <dsuarezlogans@gmail.com>
2018-08-26 11:34:42 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.get('/', (req, res) => {
+  res.status(200).send('Hello world again!!');
+});
+
+const server = require('http').createServer(app)
+server.listen(port, () => {
+  console.log('Express server listening on port ' + server.address().port);
+});
